4912a27c77562c7fce2a5017039217c250fb3945
Joe Szymkowicz <joe.szymkowicz@gmail.com>
2022-06-16 00:30:06 -0500

    Fixed not-found image

--- server/main.lua
+++ server/main.lua
@@ -700,7 +700,7 @@ QBCore.Functions.CreateCallback('mdt:server:SearchVehicles', function(source, cb
 
 				value.code = false
 				value.stolen = false
-				value.image = "img/not-found.jpg"
+				value.image = "img/not-found.webp"
 				local info = GetVehicleInformation(value.plate)
 				if info then
 					value.code = info['code5']
@@ -757,7 +757,7 @@ RegisterNetEvent('mdt:server:getVehicleData', function(plate)
 						vehicle[1]['stolen'] = info['stolen']
 					end
 
-					if vehicle[1]['image'] == nil then vehicle[1]['image'] = "img/not-found.jpg" end -- Image
+					if vehicle[1]['image'] == nil then vehicle[1]['image'] = "img/not-found.webp" end -- Image
 				end
 
 				TriggerClientEvent('mdt:client:getVehicleData', src, vehicle)
