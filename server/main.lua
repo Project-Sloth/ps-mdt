4bd764129b04a147171e3de2c9d71aa1759935be
Lenzh <lorenzo0109@hotmail.nl>
2023-04-15 11:17:28 +0200

    Fixed issue other jobs on clockin
    This fixes the issue if you had a police job and got a new job it would register to the discord webhook on playerdrop

--- server/main.lua
+++ server/main.lua
@@ -84,7 +84,7 @@ AddEventHandler('playerDropped', function(reason)
     local lastName = PlayerData.charinfo.lastname:sub(1,1):upper()..PlayerData.charinfo.lastname:sub(2)
 
     -- Auto clock out if the player is off duty
-    if PlayerData.job.onduty then
+     if IsPoliceOrEms(job) and PlayerData.job.onduty then
 		MySQL.query('UPDATE mdt_clocking SET clock_out_time = NOW(), total_time = TIMESTAMPDIFF(SECOND, clock_in_time, NOW()) WHERE user_id = @user_id ORDER BY id DESC LIMIT 1', {
 			['@user_id'] = PlayerData.citizenid
 		})
