28e6689a877b939fb608bd28fab4c54acff21ab7
BackSH00TER <18689469+BackSH00TER@users.noreply.github.com>
2023-05-14 23:54:57 -0700

    put driverslicense check behind flag

--- shared/config.lua
+++ shared/config.lua
@@ -95,6 +95,7 @@ Config.PDStations = {
 
 Config.UseWolfknightRadar = false
 Config.WolfknightNotifyTime = 5000 -- How long the notification displays for in milliseconds (30000 = 30 seconds)
+Config.PlateScanForDriversLicense = false -- If true, plate scanner will check if the owner of the scanned vehicle has a drivers license
 
 -- IMPORTANT: To avoid making excessive database queries, modify this config to true 'CONFIG.use_sonorancad = true' setting in the configuration file located at 'wk_wars2x/config.lua'. 
 -- Enabling this setting will limit plate checks to only those vehicles that have been used by a player.
