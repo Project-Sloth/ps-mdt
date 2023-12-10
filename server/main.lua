0811e45e0a857d58d6f4228d9a853f8beb47cd84
Matthew <Matthww@users.noreply.github.com>
2023-12-10 14:25:54 +0100

    Update main.lua to reflect changes of QBCore
    QBCore changed how qb-management works and added this functionality into QB-Banking

--- server/main.lua
+++ server/main.lua
@@ -1872,8 +1872,8 @@ local function giveCitationItem(src, citizenId, fine, incidentId)
 	end
 	Player.Functions.AddItem('mdtcitation', 1, false, info)
 	TriggerClientEvent('QBCore:Notify', src, PlayerName.." (" ..citizenId.. ") received a citation!")
-	if Config.QBManagementUse then 
-		exports['qb-management']:AddMoney(Officer.PlayerData.job.name, fine) 
+	if Config.QBBankingUse then 
+		exports['qb-banking']:AddMoney(Officer.PlayerData.job.name, fine) 
 	end
 	TriggerClientEvent('inventory:client:ItemBox', Player.PlayerData.source, QBCore.Shared.Items['mdtcitation'], "add")
 	TriggerEvent('mdt:server:AddLog', "A Fine was writen by "..OfficerFullName.." and was sent to "..PlayerName..", the Amount was $".. fine ..". (ID: "..incidentId.. ")")
