114c672623bf455b7e1290bddadc338963c273c9
LeSiiN <103898231+LeSiiN@users.noreply.github.com>
2026-06-23 09:25:31 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -254,7 +254,7 @@ Config.Pagination = {
     Citizens = 20, -- Citizens per page
     CitizenSearch = 20, -- Max citizen search results
     Cases = 20, -- Cases per page
-    CitizenCharges = 20, -- Charges per page in the Citizen profile's Charges section
+    CitizenCharges = 5, -- Charges per page in the Citizen profile's Charges section
 }
 
 -- Fine Processing
